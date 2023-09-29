8bbafae8c4067bcaac0e25fcfbbf2c3f98d96eb0
DeepPriyesh <priyeshdeep535@gmail.com>
2023-09-29 15:37:02 +0530

    second commint

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ mongoose
   .connect("mongodb+srv://priyeshkumar535:N1PjS9wONdoUhn9U@cluster0.hae5qck.mongodb.net/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    ssl: true 
   })
   .then(() => {
     console.log("DB Connetion Successfull");
